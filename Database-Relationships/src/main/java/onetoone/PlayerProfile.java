d052029c4adde2ac514aa7958abaa98da2bb6f90
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-08-29 12:36:17 +0300

    initial commit

---
+++ Database-Relationships/src/main/java/onetoone/PlayerProfile.java
@@ -0,0 +1,46 @@
+package onetoone;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+public class PlayerProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    private String twitter;
+    public PlayerProfile(int id, String twitter) {
+        this.Id = id;
+        this.twitter = twitter;
+    }
+    public PlayerProfile() {
+
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    @Override
+    public String toString() {
+        return "PlayerProfile{" +
+                "Id=" + Id +
+                ", twitter='" + twitter + '\'' +
+                '}';
+    }
+}
